3b31dd15346493579142083b4cec185e205882e3
paul futher <37282422+Paulfuther@users.noreply.github.com>
2023-07-31 23:00:51 -0400

    Update base.py

--- arl/arl/settings/base.py
+++ arl/arl/settings/base.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'arl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dsign',
+        'USER': 'postgres',
+        'PASSWORD': 'Paulee12!@',
+        'HOST': 'localhost'
+        
     }
 }
 
