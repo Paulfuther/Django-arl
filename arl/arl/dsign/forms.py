7203053dc40284739d0ef73029f8d3671c13afd5
paul futher <37282422+Paulfuther@users.noreply.github.com>
2023-11-29 13:00:19 -0500

    Updates to docsign

--- arl/arl/dsign/forms.py
+++ arl/arl/dsign/forms.py
@@ -6,19 +6,28 @@ from .models import DocuSignTemplate
 class NameEmailForm(forms.Form):
     name = forms.CharField(label='Name', max_length=100)
     email = forms.EmailField(label='Email')
-    template_name = forms.CharField(label='Template Name', max_length=255)
+    template_name = forms.ChoiceField(label='Template Name', choices=[])
 
-    def clean_template_name(self):
-        template_name = self.cleaned_data['template_name']
-        try:
-            template = DocuSignTemplate.objects.get(template_name=template_name)
-            self.template_id = template.template_id
-        except DocuSignTemplate.DoesNotExist:
-            raise forms.ValidationError("Template name does not exist")
-        return template_name
+    def __init__(self, *args, **kwargs):
+        super(NameEmailForm, self).__init__(*args, **kwargs)
+        self.fields['template_name'].choices = self.get_template_choices()
+
+    def get_template_choices(self):
+        choices = [('', 'Select a template')]  # Initial default choice
+        templates = DocuSignTemplate.objects.all()  # Fetch all DocuSign templates
+        for template in templates:
+            choices.append((template.template_name, template.template_name))
+        return choices
 
     def clean(self):
         cleaned_data = super().clean()
-        if 'template_name' in cleaned_data:
-            self.cleaned_data['template_id'] = getattr(self, 'template_id', None)
+        template_name = cleaned_data.get('template_name')
+
+        if template_name:
+            try:
+                template = DocuSignTemplate.objects.get(template_name=template_name)
+                cleaned_data['template_id'] = template.template_id
+            except DocuSignTemplate.DoesNotExist:
+                raise forms.ValidationError("Selected template does not exist")
+
         return cleaned_data
